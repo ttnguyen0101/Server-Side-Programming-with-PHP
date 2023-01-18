22ca6a889586b2f8a3076caf20979725f9ade8f6
Thu T Nguyen <ttnguyen18@jcu.edu>
2023-01-18 11:16:43 -0500

    initial commit to lab 1

---
+++ Lab01/nguyen-lab1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>IT 2600 Lab 1</title>
+    </head>
+    <body>
+        <h2>Thu T Nguyen - IT 2600 - Lab 1</h2>
+        <?php
+        echo "Hello World from Thu T Nguyen!";
+        ?>
+    </body>
+</html>
